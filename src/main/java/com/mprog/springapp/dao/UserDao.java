f44334364d79afd1d977dbeb03ee1483b94e3445
Munir Zikrullaev <munirprog@gmail.com>
2021-08-22 11:47:42 +0300

    implement dao

---
+++ src/main/java/com/mprog/springapp/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.mprog.springapp.dao;
+
+import com.mprog.springapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDao extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
